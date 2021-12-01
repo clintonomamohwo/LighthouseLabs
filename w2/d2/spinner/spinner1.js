654bd0e13a41e6eecc1ce1efacb7f6f147f9a68d
Ruonaclinton@gmail.com <ruonaclinton@yahoo.com>
2021-11-30 22:23:40 -0500

    Spinner I

--- w2/d2/spinner/spinner1.js
+++ w2/d2/spinner/spinner1.js
@@ -1,24 +1,4 @@
 
-//process.stdout.write('hello from spinner1.js... \rheyyy\n');
-// let x = 200;
-
-// for (let i = 0; i < 100; i++) {
-//   setTimeout(() => {
-//     process.stdout.write('\r |   ');
-//   }, x + (i * 1000));
-//   setTimeout(() => {
-//     process.stdout.write('\r /   ');
-//   }, 2 * x + (i * 1000));
-//   setTimeout(() => {
-//     process.stdout.write('\r -   ');
-//   }, 3 * x + (i * 1000));
-//   setTimeout(() => {
-//     process.stdout.write('\r \\   ');
-//   }, 4 * x + (i * 1000));
-//   setTimeout(() => {
-//     process.stdout.write('\r |   ');
-//   }, 5 * x + (i * 1000));
-// }
 
 setTimeout(() => {
   process.stdout.write('\r|   ');
